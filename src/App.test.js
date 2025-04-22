6eb61650f9f6934c6f50894463ac0f6aebaa854a
ralph <oloresralph@gmail.com>
2025-04-23 05:09:31 +1200

    Modified app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders the app without crashing', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  // Example: Look for some text you expect to be on the screen
+  const linkElement = screen.getByText(/welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
